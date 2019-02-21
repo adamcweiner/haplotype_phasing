39675c0e2f225e32e3aca151946180e6c6ca8e51
adamcweiner <adamcweiner@gmail.com>
2019-02-21 12:57:26 -0800

    move more material into node classes

--- scripts/clarks.py
+++ scripts/clarks.py
@@ -1,5 +1,6 @@
 """ Implement Clark's algorithm on a chunk to eliminate some of the haplotypes. """
 import numpy as np
+from nodes import Gen, Hap
 
 class Clark:
     """ Holds all functions relevant to Clark's algorithm. """
@@ -8,25 +9,21 @@ class Clark:
         self.chunk = chunk.T # transpose this to make chunk of shape (50, 10)... this becomes easier to index
         self.N = self.chunk.shape[0] # represents the number of genotypes collected (should be 50)
         self.K = self.chunk.shape[1] # represents the length of each genotype collected (should be size_of_chunk)
-        self.known_hap = set() # holds our set of known haplotypes
+        self.g_nodes = [] # list of all our genotype nodes
+        for n in range(self.N):
+            self.g_nodes.append(Gen(self.chunk[n])) # create a Gen object with the proper sequence and append it to the list
+        self.known_hap = [] # holds our list of known haplotype nodes
 
     def run(self):
         """ Calls proper functions to correctly run Clark's algorithm. """
         # add known haplotypes from homogeneous genotypes
         for n in range(self.N):
-            if self.is_homogeneous(self.chunk[n]):
-                temp_hap = self.find_homo_hap(self.chunk[n])
+            if self.g_nodes[n].is_homogeneous():
+                temp_hap = self.find_homo_hap(self.g_nodes[n])
                 self.known_hap.add(temp_hap) # append the haplotype to the known list
         print(self.known_hap)
 
-    def is_homogeneous(self, gen):
-        """ Returns True if the genotype only consists of 0's and 2's; returns False if any of the positions are heterogeneous. """
-        temp = True
-        for ii in range(len(gen)):
-            if gen[ii] == 1: # if element is 1, break and return False
-                temp = False
-                break
-        return temp
+
 
     def find_homo_hap(self, gen):
         """ Gives the haplotype of a homogenous genotype. All g[i]=2's turn into h[i]=1's and all g[i]=0's turn into h[i]=0's. """
--- scripts/nodes.py
+++ scripts/nodes.py
@@ -12,16 +12,25 @@ class Gen:
         """ Adds the given haplotype and assigns the other haplotype haplotype assigned to the genotype. Note that h1 is a Hap object. """
         assert self.valid_hap(h1.seq)
         self.h1 = h1
-        h2_seq = self.complementary_hap(h1, self.seq) # find sequence of second haplotype by finding complement
+        h2_seq = self.complementary_hap(h1.seq) # find sequence of second haplotype by finding complement
         self.h2 = Hap(h2_seq) # create Hap object given the proper sequence
 
-    def complementary_hap(self, haplotype, genotype):
-        """ Given a genotype and a haplotype, find the haplotype that complements the one given. """
-        assert len(haplotype) == len(genotype) # make sure the two arrays are of the same size
-        comp_hap = np.zeros((len(haplotype))) # fill comp_hap with just zeros to start
-        for ii in range(len(comp_hap)):
-            comp_hap[ii] = genotype[ii] - haplotype[ii] # i.e. if g[ii] = 2 and h[ii] = 1 then c_h[ii] = 1
+    def complementary_hap(self, hap_seq):
+        """ Given genotype and a haplotype sequences, find the haplotype sequence that complements the one given. """
+        assert len(hap_seq) == len(self.seq) # make sure the two arrays are of the same size
+        comp_hap = np.zeros((len(hap_seq))) # fill comp_hap with just zeros to start
+        for ii in range(len(hap_seq)):
+            comp_hap[ii] = self.seq[ii] - hap_seq[ii] # i.e. if g[ii] = 2 and h[ii] = 1 then c_h[ii] = 1
         return comp_hap
+
+    def is_homogeneous(self):
+        """ Returns True if the genotype only consists of 0's and 2's; returns False if any of the positions are heterogeneous. """
+        temp = True
+        for ii in range(len(self.seq)):
+            if self.seq[ii] == 1: # if element is 1, break and return False
+                temp = False
+                break
+        return temp
 
 class Hap:
     """ Creates a haplotype object that has a pointer to its genotype. """
