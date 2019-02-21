284cd5025aa3b9effc031f052a199f78794f2211
adamcweiner <adamcweiner@gmail.com>
2019-02-21 15:55:37 -0800

    this is a shitshow

--- scripts/clarks.py
+++ scripts/clarks.py
@@ -18,19 +18,41 @@ class Clark:
         """ Calls proper functions to correctly run Clark's algorithm. """
         # add known haplotypes from homogeneous genotypes
         for n in range(self.N):
-            if self.g_nodes[n].is_homogeneous():
-                temp_hap = self.find_homo_hap(self.g_nodes[n])
-                self.known_hap.add(temp_hap) # append the haplotype to the known list
+            if self.g_nodes[n].n_ones == 0: # if the sequence is homogenous (all 0's and 2's)
+                temp_hap_seq = self.find_homo_hap(self.g_nodes[n].seq)
+                temp_ptr = self.unique_haplo(temp_hap_seq)
+                if temp_ptr is None: # when temp_hap_seq is truly unique
+                    temp_hap_node = Hap(temp_hap_seq) # create new node (h1)
+                    g_node[n].add_hs(temp_hap_node) # add ptr to h1 and create+link h2.... TODO: ensure that this doesn't double count for homogenous genotypes
+                    self.known_hap.append(temp_hap_node) # append new haplotype to the known_hap list
+                # if temp_hap_seq is found as the sequence for a node in known_hap
+                    # have the genotype point to this existing haplotype node
+                # else
+                    # create a new node according to the sequence
+                    # have the g_node[n] point to this haplotype
+                    # append new haplotype to the known_hap list
+                temp_hap_node = Hap(temp_hap_seq) # create node
+                self.known_hap.add(temp_hap) # append the haplotype node to the known list
+            
         print(self.known_hap)
 
 
-
-    def find_homo_hap(self, gen):
+    def find_homo_hap(self, gen_seq):
         """ Gives the haplotype of a homogenous genotype. All g[i]=2's turn into h[i]=1's and all g[i]=0's turn into h[i]=0's. """
-        hap = np.zeros((len(gen))) # start with an array of 0's
-        for ii in range(len(gen)):
-            assert gen[ii] == 0 or gen[ii] == 2 # make sure it's a 0 or 2
-            if gen[ii] == 2:
-                hap[ii] = 1
-        return hap
+        hap_seq = np.zeros((len(gen_seq))) # start with an array of 0's
+        for ii in range(len(gen_seq)):
+            assert gen_seq[ii] == 0 or gen_seq[ii] == 2 # make sure it's a 0 or 2
+            if gen_seq[ii] == 2:
+                hap_seq[ii] = 1
+        return hap_seq
         
+
+    # need to create a function that checks if a haplotype sequence is unique compared to all that is found in self.known_hap
+    def unique_haplo(self, hap_seq):
+        """ Looks through all of self.known_hap and ensures that hap_seq is unique from all of the known haplotypes currently in the list. If the sequence already exists, it will return the address of the existing node. If the sequence is unique, it will return None. """
+        temp = None
+        for hap_node in self.known_hap:
+            if np.array_equal(hap_node.seq, hap_seq): # returns true if two arrays are equal
+                temp = hap_node # assign temp to current node and break out
+                break
+        return temp
--- scripts/nodes.py
+++ scripts/nodes.py
@@ -7,13 +7,14 @@ class Gen:
         self.seq = gen_seq
         self.h1 = None
         self.h2 = None
+        self.n_ones = self.num_het() # the number of ones in the genotype seq
 
     def add_hs(self, h1):
         """ Adds the given haplotype and assigns the other haplotype haplotype assigned to the genotype. Note that h1 is a Hap object. """
-        assert self.valid_hap(h1.seq)
         self.h1 = h1
         h2_seq = self.complementary_hap(h1.seq) # find sequence of second haplotype by finding complement
         self.h2 = Hap(h2_seq) # create Hap object given the proper sequence
+        return self.h2 # return the complementary node in case it needs to be used
 
     def complementary_hap(self, hap_seq):
         """ Given genotype and a haplotype sequences, find the haplotype sequence that complements the one given. """
@@ -23,14 +24,13 @@ class Gen:
             comp_hap[ii] = self.seq[ii] - hap_seq[ii] # i.e. if g[ii] = 2 and h[ii] = 1 then c_h[ii] = 1
         return comp_hap
 
-    def is_homogeneous(self):
-        """ Returns True if the genotype only consists of 0's and 2's; returns False if any of the positions are heterogeneous. """
-        temp = True
+    def num_het(self):
+        """ Returns the number of heterogeneous positions (1's) in the genotype sequence. """
+        count = 0
         for ii in range(len(self.seq)):
-            if self.seq[ii] == 1: # if element is 1, break and return False
-                temp = False
-                break
-        return temp
+            if self.seq[ii] == 1: # add 1 to count if hetero site encountered
+                count += 1
+        return count
 
 class Hap:
     """ Creates a haplotype object that has a pointer to its genotype. """
