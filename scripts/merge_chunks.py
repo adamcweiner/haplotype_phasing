7f5cb70bd7c337cf367483a5ef763c8d892eaa5a
Russell Littman <russelllittman@addr187.net103.lifesci.ucla.edu>
2019-03-06 10:36:54 -0800

    added list comparison

--- scripts/merge_chunks.py
+++ scripts/merge_chunks.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+from time import time
 
 def generate_random():
 	overlap = 5
@@ -11,9 +12,9 @@ def generate_random():
 		start.append(position)
 		end.append(position+19)
 		chunk = []
-		for j in range(20):
+		for j in range(10):
 			row = []
-			for i in range(100):
+			for i in range(10):
 				row.append(random.randint(0,1))
 			chunk.append(row)
 			position += 1
@@ -27,13 +28,24 @@ def generate_random():
 		test_data[i][0:overlap] = test_data[i-1][len(test_data[i-1])-overlap:len(test_data[i-1])]		
 	return test_data, start, end
 		
+
+def count_match(lst1, lst2):
+	count = 0
+	length = len(lst1)
+
+	for i in range(length):
+		if(lst1[i]==lst2[i]):
+			count+=1
+
+	return count
+
+
 #function merge_chunks(haplotype_chunks)
 #input:
 #	haplotype_chunks: list of the nx2m matrices, start positions, end positions
 #output:
 #	haplotypes: one 2mxn matrix where n is the number of total snps
 def merge_chunks(data, start, end):
-	
 	people = []
 	for i in range(len(data[0][0])):
 		person = [item[i] for item in data[0]]
@@ -42,12 +54,17 @@ def merge_chunks(data, start, end):
 	for i in range(1,len(data)):
 		overlap = end[i-1] - start[i] +1
 		for j in range(0,len(data[i-1][0]),2):
-			lst1 = [item[0] for item in data[i-1][len(data[i-1])-5:len(data[i-1])]]
-			lst2 = [item[0] for item in data[i][0:5]]
-			if(lst1 == lst2):
+			lst1 = [item[j] for item in data[i-1][len(data[i-1])-overlap:len(data[i-1])]] #ending overlapping sequence of first haplotype in a group
+			lst2 = [item[j] for item in data[i][0:overlap]] #beginning overlapping sequence of first haplotype
+			lst3 = [item[j+1] for item in data[i][0:overlap]] #beginning overlapping sequence of second haplotype
+			lst1v2 = count_match(lst1, lst2)
+			lst1v3 = count_match(lst1,lst3)
+			
+			if(lst1v2 > lst1v3): #puts the first two together
 				people[j] += [item[j] for item in data[i][overlap:len(data[i])]]
 				people[j+1] += [item[j] for item in data[i][overlap:len(data[i])]]
-			else:
+			else: #puts the first haplotype with second
+			#note that if they are matching it will default to this but shouldn't matter either way because it would be 50/50
 				people[j+1] += [item[j] for item in data[i][overlap:len(data[i])]]
 				people[j] += [item[j] for item in data[i][overlap:len(data[i])]]
 				
@@ -56,3 +73,4 @@ def merge_chunks(data, start, end):
 
 #data, start, end = generate_random()
 #merge_chunks(data,start,end)
+
