7463816132c1df70eca19d2ccbc4ab0528341873
Cyrillus Tan <cyztan@gmail.com>
2019-03-05 17:50:43 -0800

    Update em_algorithm2.py

--- scripts/em_algorithm2.py
+++ scripts/em_algorithm2.py
@@ -41,8 +41,10 @@ def em_algorithm2(data):
 			n_sol = 1 if n_ones<=0 else 2**n_ones
 		indprob.append([1/n_sol] * n_sol)
 		indptr.append([[] for _ in range(n_sol)])
-		seq1 = np.array([1 if i >= 1 else 0 for i in ind_geno1])
-		seq2 = np.array([1 if i >= 1 else 0 for i in ind_geno2])
+		if n_ones <= 0:
+			pool_add(ind_geno1.copy(), ind, 0)
+			pool_add(ind_geno2.copy(), ind, 0)
+			continue
 		lst = list(product([0,1], repeat=n_ones-1 if no_nequal_spot else n_ones))
 		for idx, comb in enumerate(lst):
 			if no_nequal_spot:
@@ -51,11 +53,12 @@ def em_algorithm2(data):
 			else:
 				comb_lst = np.array(list(comb))
 				compl_lst = [1]*n_ones-comb_lst
-			seq1[unphased_spot] = comb_lst
-			seq2[unphased_spot] = compl_lst
-			pool_add(seq1.copy(), ind, idx)
-			pool_add(seq2.copy(), ind, idx)
+			ind_geno1[unphased_spot] = comb_lst
+			ind_geno2[unphased_spot] = compl_lst
+			pool_add(ind_geno1.copy(), ind, idx)
+			pool_add(ind_geno2.copy(), ind, idx)
 	poolprob = poolprob / np.sum(poolprob)
+	print('Pool size ', len(poolprob))
 
 	## Iterations
 	num_iter = 0
@@ -73,11 +76,8 @@ def em_algorithm2(data):
 		for iind in range(len(indprob)):
 			for isol in range(len(indprob[iind])):
 				p1 = indptr[iind][isol][0]
-				if len(indptr[iind][isol])>1:
-					p2 = indptr[iind][isol][1]
-					indprob[iind][isol] *= poolprob[p1]*poolprob[p2]
-				else:
-					indprob[iind][isol] *= poolprob[p1]*poolprob[p1]
+				p2 = indptr[iind][isol][1]
+				indprob[iind][isol] *= poolprob[p1]*poolprob[p2]
 			indprob[iind] = indprob[iind]/sum(indprob[iind])
 		## Test convergence
 		indconv = [np.any((np.array(prob)>0.99)==True) for prob in indprob]
@@ -90,6 +90,6 @@ def em_algorithm2(data):
 	for ind in range(m_ind):
 		pos = np.argmax(indprob[ind])
 		chrom1 = pool[indptr[ind][pos][0]]
-		chrom2 = pool[indptr[ind][pos][0]] if len(indptr[ind][pos])<2 else pool[indptr[ind][pos][1]]
-		res.append(np.vstack((chrom1.copy(), chrom2.copy())).T)		
+		chrom2 = pool[indptr[ind][pos][1]]
+		res.append(np.vstack((chrom1.copy(), chrom2.copy())).T)
 	return np.hstack(res)
